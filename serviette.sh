2b891123f44fbb9b118649970a2301b6a4bbad11
Daniel Weuthen <daniel@weuthen-net.de>
2015-02-07 21:40:29 +0100

    turned former txt file into shell script and renamed it added more comments

--- serviette.sh
+++ serviette.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+#################################################
+# General 
+#################################################
+
 # Set hostname
 echo serviette > /etc/hostname
 
@@ -30,18 +36,20 @@ aptitude update && aptitude -y upgrade
 rm /etc/ssh/*.pub /etc/ssh/*_key
 dpkg-reconfigure openssh-server
 
-# Install basic tools
-aptitude -y install zsh vim less gzip git-core curl python g++ iw wpasupplicant wireless-tools dnsmasq bridge-utils screen tmux mosh ed strace cowsay figlet toilet at  pv mmv iputils-tracepath tre-agrep urlscan urlview autossh elinks irssi-scripts ncftp sc byobu mc tree atop iftop iotop nmap antiword moreutils net-tools whois pwgen
-
 # Set time zone
 echo "Europe/Berlin" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
+# Install basic tools
+aptitude -y install zsh vim less gzip git-core curl python g++ iw wpasupplicant wireless-tools bridge-utils screen tmux mosh ed strace cowsay figlet toilet at pv mmv iputils-tracepath tre-agrep urlscan urlview autossh elinks irssi-scripts ncftp sc byobu mc tree atop iftop iotop nmap antiword moreutils net-tools whois pwgen haveged
 
-# Firmware for rt2800usb 
+
+#################################################
+# Network Settings
+#################################################
+
+# Firmware for rt2800usb (USB WiFi) 
 aptitude install firmware-ralink
-
-== Network Settings ==
 
 cat > /etc/network/interfaces <<EOF
 # interfaces(5) file used by ifup(8) and ifdown (8)                                                                                                                                  
@@ -89,12 +97,16 @@ iptables -A FORWARD -p TCP --dport 11371 -j REJECT
 exit 0
 EOF
 
-== ETHERPAD-LITE ==
-# NodeJS installieren
+
+#################################################
+# ETHERPAD-LITE
+#################################################
+
+# Install NodeJS
 curl -sL https://deb.nodesource.com/setup | bash -
 aptitude install -y nodejs
 
-# Etherpad-lite installieren
+# Install Etherpad-lite
 useradd -m etherpad
 su etherpad -s /bin/bash
 git clone git://github.com/ether/etherpad-lite.git
@@ -104,21 +116,32 @@ npm install ep_adminpads
 npm install ep_markdown
 npm install ep_markdownify
 
+echo "Please enable an admin user in the Etherpad-lite configuration"
+read -p "Press [Enter] key to open Etherpad-lite configuration file..."
+
 vim ~/etherpad-lite/settings.json
 
-# admin user anlegen
+exit
 
+# Install and configure Etherpad-lite init script
 wget https://github.com/serviette/serviette/raw/master/etherpad-lite.init -O /etc/init.d/etherpad-lite 
-
 chmod +x /etc/init.d/etherpad-lite
 mkdir /var/log/etherpad-lite
 chown etherpad:etherpad /var/log/etherpad-lite/
 update-rc.d etherpad-lite defaults
+
+# Start Etherpad-lite
 service etherpad-lite start
 
-== XMPP Server ==
+
+#################################################
+# XMPP Server
+#################################################
+
+# Install Prosody XMPP server
 aptitude -y install prosody
 
+# Create Prosody configuration
 cat > /etc/prosody/conf.d/serviette.cfg.lua <<EOF
 VirtualHost "serviette.lan"
     allow_registration = true
@@ -126,51 +149,87 @@ VirtualHost "serviette.lan"
     groups_file = "/etc/prosody/serviette_groups.txt"
 EOF
 
+# Download Prosody PAM authentication module
 wget https://raw.githubusercontent.com/augustf/prosody-mod_auth_pam/master/mod_auth_pam.lua -O /usr/lib/prosody/modules/mod_auth_pam.lua
 
+# Enable Prosody groups support
 sed -i 's/--"groups"/"groups"/' /etc/prosody/prosody.cfg.lua 
 
+# Restart Prosody to adopt changes
 service prosody restart
 
-== PGP/GPG Key Server ==
+
+#################################################
+# PGP/GPG Key Server
+#################################################
+
+# Install Synchronizing OpenPGP Key Server
 aptitude -y install sks
+
+# Initialize Synchronizing OpenPGP Key Server database
 sks build
 chown -Rc debian-sks:debian-sks /var/lib/sks/DB
+
+# Disable communication with other key servers
 echo '# Empty - Do not communicate with other keyservers.' >/etc/sks/mailsync
 echo '# Empty - Do not communicate with other keyservers.' >/etc/sks/membership
+
+# Enable start via init script
 echo 'initstart=yes' >/etc/default/sks
+
+# Start Synchronizing OpenPGP Key Server
 service sks start
 
-== Webserver ==
 
+#################################################
+#  HTTP Server
+#################################################
+
+# Install Nginx, FastCGI Wrapper and PHP5 (CGI)
 aptitude -y install nginx-light fcgiwrap php5-cgi
 
-service nginx restart
-
+# Make sure that every new users gets his own public_html
 mkdir /etc/skel/public_html
 
-
+# Allow HTTP server user to create new users, required for self-service portal
 cat > /etc/sudoers <<EOF
 www-data ALL=(root) NOPASSWD: /usr/sbin/useradd"
 EOF
 
+# Restart Nginx to adopt changes 
+service nginx restart
 
-== Ikiwiki ==
+
+#################################################
+# Ikiwiki
+#################################################
+
+# Install IkiWiki
 aptitude -y install ikiwiki
 
 
-== IRC Server ==
+#################################################
+# IRC Server
+#################################################
+
+# Install ngIRCd
 aptitude -y install ngircd
 
+# Configure hostname if IRC server
 sed -i 's/Name = irc.example.net/Name = irc.serviette.lan/' /etc/ngircd/ngircd.conf
 
+# Restart ngIRCd to adopt changes
 service ngircd restart
 
 
-== HostAPd ==
-aptitude -y install hostapd
-sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+#################################################
+# Wireless Access Point
+#################################################
 
+# Install HostAPd
+aptitude -y install hostapd
+
+# Create cHostAPd configuration file
 cat - << EOF > /etc/hostapd/hostapd.conf
 interface=wlan1
 bridge=br0
@@ -188,25 +247,42 @@ auth_algs=1
 macaddr_acl=0
 EOF
 
+# Specify configuration file
+sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+
+# Restart HostAPd to adopt changes
 service hostapd restart
 
 
-== DNSMASQ ==
+#################################################
+# DNS & DHCP Server
+#################################################
+
+# Install Dnsmasq
+aptitude -y install dnsmasq
+
+# Create Dnsmasq configuration
 cat - << EOF > /etc/dnsmasq.conf
 interface=wlan1
 domain=serviette.lan
 dhcp-range=192.168.0.50,192.168.0.150,12h
 EOF
 
+# Restart Dnsmasq to adopt changes
 service dnsmasq restart
 
 
-== SharingIsCaring ==
+#################################################
+#  SharingIsCaring
+#################################################
+
+# Install SharingIsCaring
 useradd -m sharingiscaring
 su sharingiscaring -s /bin/bash
 cd ~
 npm install git://github.com/c3d2/sharingiscaring.git
 
+# Create executable file for SharingIsCaring
 cat - << EOF > /home/sharingiscaring/run.sh
 #!/bin/bash
 
@@ -215,16 +291,28 @@ export PORT=8090
 ./node_modules/.bin/sharingiscaring
 EOF
 
-wget https://github.com/serviette/serviette/raw/master/sharingiscaring.init -O /etc/init.d/sharingiscaring
+chmod +x /home/sharingiscaring/run.sh
+exit
 
+# Install and configure SharingIsCaring init script
+wget https://github.com/serviette/serviette/raw/master/sharingiscaring.init -O /etc/init.d/sharingiscaring
+chmod +x /etc/init.d/sharingiscaring
+mkdir /var/log/sharingiscaring
+chown sharingiscaring:sharingiscaring /var/log/sharingiscaring/
 update-rc.d sharingiscaring defaults
 
+# Start SharingIsCaring
 service sharingiscaring start
 
-== Email ==
 
-aptitude install exim4-light dovecot-imapd mutt haveged
+#################################################
+# SMTP & IMAP Server/Client
+################################################# 
 
+# Install Exim, Dovecot and Mutt
+aptitude -y install exim4-light dovecot-imapd mutt
+
+# Create Exim configuration the Debian way
 cat - << EOF > /etc/exim4/update-exim4.conf.conf
 dc_eximconfig_configtype='local'
 dc_other_hostnames='serviette.lan'
@@ -241,6 +329,8 @@ dc_mailname_in_oh='true'
 dc_localdelivery='mail_spool'
 EOF
 
+# Create Exim configuration from configuration template
 update-exim4.conf
 
-
+# Restart Exim to adopt changes
+service exim4 restart
