b6a0f6f03425652fc9c7b2c097a85a386289297d
Daniel Weuthen <daniel@weuthen-net.de>
2015-03-17 00:14:49 +0100

    split installation steps into functions added nginx vhosts

--- serviette.sh
+++ serviette.sh
@@ -1,57 +1,62 @@
-#!/bin/sh
+#!/bin/bash
 
 #################################################
 # General 
 #################################################
 
-# Set hostname
-echo serviette > /etc/hostname
-
-# Allow every user to use DNS resolution                                                                                                                                            
-chmod a+r /etc/resolv.conf
-
-# Configure locales
-aptitude -y install locales
-
-cat > /etc/locale.gen <<EOF 
+function install_base {
+    # Set hostname
+    echo serviette > /etc/hostname
+    
+    # Allow every user to use DNS resolution                                                                                                                                            
+    chmod a+r /etc/resolv.conf
+    
+    # Update package list
+    aptitude -y update
+    
+    # Configure locales
+    aptitude -y install locales
+    
+    cat > /etc/locale.gen <<EOF 
 de_AT.UTF-8 UTF-8
 de_CH.UTF-8 UTF-8
 de_DE.UTF-8 UTF-8
 en_US.UTF-8 UTF-8
 EOF
-
-locale-gen
-
-# Set up Debian's sources.list
-cat > /etc/apt/sources.list <<EOL
+    
+    locale-gen
+    
+    # Set up Debian's sources.list
+    cat > /etc/apt/sources.list <<EOL
 deb http://ftp.de.debian.org/debian wheezy main contrib non-free
 deb http://ftp.de.debian.org/debian wheezy-updates main contrib non-free
 deb http://ftp.de.debian.org/debian-security wheezy/updates main contrib non-free
 EOL
-
-# Install latest system updates
-aptitude update && aptitude -y upgrade
-
-# Reset SSH hostkeys
-rm /etc/ssh/*.pub /etc/ssh/*_key
-dpkg-reconfigure openssh-server
-
-# Set time zone
-echo "Europe/Berlin" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
-
-# Install basic tools
-aptitude -y install zsh vim less gzip git-core curl python g++ iw wpasupplicant wireless-tools bridge-utils screen tmux mosh ed strace cowsay figlet toilet at pv mmv iputils-tracepath tre-agrep urlscan urlview autossh elinks irssi-scripts ncftp sc byobu mc tree atop iftop iotop nmap antiword moreutils net-tools whois pwgen haveged
-
+    
+    # Install latest system updates
+    aptitude update && aptitude -y upgrade
+    
+    # Reset SSH hostkeys
+    rm /etc/ssh/*.pub /etc/ssh/*_key
+    dpkg-reconfigure openssh-server
+    
+    # Set time zone
+    echo "Europe/Berlin" > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
+    
+    # Install basic tools
+    aptitude -y install zsh vim less gzip git-core curl python g++ iw wpasupplicant wireless-tools bridge-utils screen tmux mosh ed strace cowsay figlet toilet at pv mmv iputils-tracepath tre-agrep urlscan urlview autossh elinks irssi-scripts ncftp sc byobu mc tree atop iftop iotop nmap antiword moreutils net-tools whois pwgen haveged
+}
 
 #################################################
 # Network Settings
 #################################################
 
-# Firmware for rt2800usb (USB WiFi) 
-aptitude install firmware-ralink
+function configure_network {
+    # Firmware for rt2800usb (USB WiFi) 
+    aptitude install firmware-ralink
 
-cat > /etc/network/interfaces <<EOF
+    cat > /etc/network/interfaces <<EOF
 # interfaces(5) file used by ifup(8) and ifdown (8)                                                                                                                                  
 auto lo
 iface lo inet loopback
@@ -74,10 +79,10 @@ iface wlan1 inet static
 iface default inet dhcp
 EOF
 
-# Make some basic settings at boot time
-sed -i '$ d' /etc/rc.local
+    # Make some basic settings at boot time
+    sed -i '$ d' /etc/rc.local
 
-cat > /etc/rc.local <<EOF
+    cat > /etc/rc.local <<EOF
 # Make the blue LED only flash on activity on SD card
 echo mmc0 > /sys/class/leds/led1/trigger
 
@@ -96,141 +101,167 @@ iptables -A FORWARD -p TCP --dport 11371 -j REJECT
 
 exit 0
 EOF
-
+}
 
 #################################################
 # ETHERPAD-LITE
 #################################################
 
-# Install NodeJS
-curl -sL https://deb.nodesource.com/setup | bash -
-aptitude install -y nodejs
+function install_etherpad {
+    # Install NodeJS
+    curl -sL https://deb.nodesource.com/setup | bash -
+    aptitude install -y nodejs
 
-# Install Etherpad-lite
-useradd -m etherpad
-su etherpad -s /bin/bash
-git clone git://github.com/ether/etherpad-lite.git
-cd ~/etherpad-lite
-npm install ep_list_pads
-npm install ep_adminpads
-npm install ep_markdown
-npm install ep_markdownify
+    # Install Etherpad-lite
+    useradd -m etherpad
+    su etherpad -c "cd ~ && git clone git://github.com/ether/etherpad-lite.git"
+    su etherpad -c "cd ~/etherpad-lite && npm install ep_list_pads && npm install ep_adminpads && npm install ep_markdown && npm install ep_markdownify"
+    
 
-echo "Please enable an admin user in the Etherpad-lite configuration"
-read -p "Press [Enter] key to open Etherpad-lite configuration file..."
+    su etherpad -c "cp ~/etherpad-lite/settings.json.template ~/etherpad-lite/settings.json"
+    echo "Please enable an admin user in the Etherpad-lite configuration"
+    read -p "Press [Enter] key to open Etherpad-lite configuration file..."
+    
+    
+    su etherpad -c "vim ~/etherpad-lite/settings.json"
+    
+    # Install and configure Etherpad-lite init script
+    wget https://github.com/serviette/serviette/raw/master/etherpad-lite.init -O /etc/init.d/etherpad-lite 
+    chmod +x /etc/init.d/etherpad-lite
+    mkdir /var/log/etherpad-lite
+    chown etherpad:etherpad /var/log/etherpad-lite/
+    update-rc.d etherpad-lite defaults
+    
+    # Start Etherpad-lite
+    service etherpad-lite start
 
-vim ~/etherpad-lite/settings.json
+    # Create and endable virtual host
+    cat > /etc/nginx/sites-available/pads.serviette.lan <<EOF
+server{
+  server_name pads.serviette.lan;
+  location / {
+    proxy_pass http://127.0.0.1:9001;
+  }
+}
+EOF
 
-exit
-
-# Install and configure Etherpad-lite init script
-wget https://github.com/serviette/serviette/raw/master/etherpad-lite.init -O /etc/init.d/etherpad-lite 
-chmod +x /etc/init.d/etherpad-lite
-mkdir /var/log/etherpad-lite
-chown etherpad:etherpad /var/log/etherpad-lite/
-update-rc.d etherpad-lite defaults
-
-# Start Etherpad-lite
-service etherpad-lite start
-
+    ln -s /etc/nginx/sites-available/pads.serviette.lan /etc/nginx/sites-enabled/pads.serviette.lan
+}
 
 #################################################
 # XMPP Server
 #################################################
 
-# Install Prosody XMPP server
-aptitude -y install prosody
-
-# Create Prosody configuration
-cat > /etc/prosody/conf.d/serviette.cfg.lua <<EOF
+function install_prosody {
+    # Install Prosody XMPP server
+    aptitude -y install prosody
+    
+    # Create Prosody configuration
+    cat > /etc/prosody/conf.d/serviette.cfg.lua <<EOF
 VirtualHost "serviette.lan"
     allow_registration = true
     authentication = "internal_hashed"
     groups_file = "/etc/prosody/serviette_groups.txt"
 EOF
-
-# Download Prosody PAM authentication module
-wget https://raw.githubusercontent.com/augustf/prosody-mod_auth_pam/master/mod_auth_pam.lua -O /usr/lib/prosody/modules/mod_auth_pam.lua
-
-# Enable Prosody groups support
-sed -i 's/--"groups"/"groups"/' /etc/prosody/prosody.cfg.lua 
-
-# Restart Prosody to adopt changes
-service prosody restart
-
+    
+    # Download Prosody PAM authentication module
+    wget https://raw.githubusercontent.com/augustf/prosody-mod_auth_pam/master/mod_auth_pam.lua -O /usr/lib/prosody/modules/mod_auth_pam.lua
+    
+    # Enable Prosody groups support
+    sed -i 's/--"groups"/"groups"/' /etc/prosody/prosody.cfg.lua 
+    
+    # Restart Prosody to adopt changes
+    service prosody restart
+}
 
 #################################################
 # PGP/GPG Key Server
 #################################################
 
-# Install Synchronizing OpenPGP Key Server
-aptitude -y install sks
+function install_sks {
+    # Install Synchronizing OpenPGP Key Server
+    aptitude -y install sks
+    
+    # Initialize Synchronizing OpenPGP Key Server database
+    sks build
+    chown -Rc debian-sks:debian-sks /var/lib/sks/DB
+    
+    # Disable communication with other key servers
+    echo '# Empty - Do not communicate with other keyservers.' > /etc/sks/mailsync
+    echo '# Empty - Do not communicate with other keyservers.' > /etc/sks/membership
+    
+    # Enable start via init script
+    echo 'initstart=yes' > /etc/default/sks
+    
+    # Start Synchronizing OpenPGP Key Server
+    service sks start
 
-# Initialize Synchronizing OpenPGP Key Server database
-sks build
-chown -Rc debian-sks:debian-sks /var/lib/sks/DB
+    cat > /etc/nginx/sites-available/keyserver.serviette.lan <<EOF
+server{
+  server_name keyserver.serviette.lan;
+  location / {
+    proxy_pass http://127.0.0.1:11371;
+  }
+}
+EOF
 
-# Disable communication with other key servers
-echo '# Empty - Do not communicate with other keyservers.' >/etc/sks/mailsync
-echo '# Empty - Do not communicate with other keyservers.' >/etc/sks/membership
-
-# Enable start via init script
-echo 'initstart=yes' >/etc/default/sks
-
-# Start Synchronizing OpenPGP Key Server
-service sks start
-
+    ln -s /etc/nginx/sites-available/keyserver.serviette.lan .
+}
 
 #################################################
 #  HTTP Server
 #################################################
 
-# Install Nginx, FastCGI Wrapper and PHP5 (CGI)
-aptitude -y install nginx-light fcgiwrap php5-cgi
+function install_httpd {
+    # Install Nginx, FastCGI Wrapper and PHP5 (CGI)
+    aptitude -y install nginx-light fcgiwrap php5-cgi
 
-# Make sure that every new users gets his own public_html
-mkdir /etc/skel/public_html
+    # Make sure that every new users gets his own public_html
+    mkdir /etc/skel/public_html
 
-# Allow HTTP server user to create new users, required for self-service portal
-cat > /etc/sudoers <<EOF
+    # Allow HTTP server user to create new users, required for self-service portal
+    cat > /etc/sudoers <<EOF
 www-data ALL=(root) NOPASSWD: /usr/sbin/useradd"
 EOF
 
-# Restart Nginx to adopt changes 
-service nginx restart
-
+    # Restart Nginx to adopt changes 
+    service nginx restart
+}
 
 #################################################
 # Ikiwiki
 #################################################
 
-# Install IkiWiki
-aptitude -y install ikiwiki
-
+function install_ikiwiki {
+    # Install IkiWiki
+    aptitude -y install ikiwiki
+}
 
 #################################################
 # IRC Server
 #################################################
 
-# Install ngIRCd
-aptitude -y install ngircd
-
-# Configure hostname if IRC server
-sed -i 's/Name = irc.example.net/Name = irc.serviette.lan/' /etc/ngircd/ngircd.conf
-
-# Restart ngIRCd to adopt changes
-service ngircd restart
-
+function install_ngircd {
+    # Install ngIRCd
+    aptitude -y install ngircd
+    
+    # Configure hostname if IRC server
+    sed -i 's/Name = irc.example.net/Name = irc.serviette.lan/' /etc/ngircd/ngircd.conf
+    
+    # Restart ngIRCd to adopt changes
+    service ngircd restart
+}
 
 #################################################
 # Wireless Access Point
 #################################################
 
-# Install HostAPd
-aptitude -y install hostapd
-
-# Create cHostAPd configuration file
-cat - << EOF > /etc/hostapd/hostapd.conf
+function install_hostapd {
+    # Install HostAPd
+    aptitude -y install hostapd
+    
+    # Create cHostAPd configuration file
+    cat - << EOF > /etc/hostapd/hostapd.conf
 interface=wlan1
 bridge=br0
 driver=nl80211
@@ -246,74 +277,88 @@ rsn_pairwise=CCMP
 auth_algs=1
 macaddr_acl=0
 EOF
-
-# Specify configuration file
-sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
-
-# Restart HostAPd to adopt changes
-service hostapd restart
-
+    
+    # Specify configuration file
+    sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+    
+    # Restart HostAPd to adopt changes
+    service hostapd restart
+}    
 
 #################################################
 # DNS & DHCP Server
 #################################################
 
-# Install Dnsmasq
-aptitude -y install dnsmasq
-
-# Create Dnsmasq configuration
-cat - << EOF > /etc/dnsmasq.conf
+function install_dnsmasq {
+    # Install Dnsmasq
+    aptitude -y install dnsmasq
+    
+    # Create Dnsmasq configuration
+    cat - << EOF > /etc/dnsmasq.conf
 interface=wlan1
 domain=serviette.lan
 dhcp-range=192.168.0.50,192.168.0.150,12h
 EOF
-
-# Restart Dnsmasq to adopt changes
-service dnsmasq restart
-
+    
+    # Restart Dnsmasq to adopt changes
+    service dnsmasq restart
+}
 
 #################################################
 #  SharingIsCaring
 #################################################
 
-# Install SharingIsCaring
-useradd -m sharingiscaring
-su sharingiscaring -s /bin/bash
-cd ~
-npm install git://github.com/c3d2/sharingiscaring.git
-
-# Create executable file for SharingIsCaring
-cat - << EOF > /home/sharingiscaring/run.sh
+function install_sharingiscaring {
+    # Install SharingIsCaring
+    useradd -m sharingiscaring
+    su sharingiscaring -c "cd ~ && npm install git://github.com/c3d2/sharingiscaring.git"
+    
+    # Create executable file for SharingIsCaring
+    cat - << EOF > /home/sharingiscaring/run.sh
 #!/bin/bash
 
-cd $HOME
+cd ~
 export PORT=8090
 ./node_modules/.bin/sharingiscaring
 EOF
+    
+    chown sharingiscaring:sharingiscaring /home/sharingiscaring/run.sh
+    chmod +x /home/sharingiscaring/run.sh
+    
+    # Install and configure SharingIsCaring init script
+    wget https://github.com/serviette/serviette/raw/master/sharingiscaring.init -O /etc/init.d/sharingiscaring
+    chmod +x /etc/init.d/sharingiscaring
+    mkdir /var/log/sharingiscaring
+    chown sharingiscaring:sharingiscaring /var/log/sharingiscaring/
+    update-rc.d sharingiscaring defaults
+    
+    # Start SharingIsCaring
+    service sharingiscaring start
 
-chmod +x /home/sharingiscaring/run.sh
-exit
+    # Create and endable virtual host
+    cat > /etc/nginx/sites-available/sharing.serviette.lan <<EOF
+server{
+  server_name sharing.serviette.lan;
+  location / {
+    proxy_pass http://127.0.0.1:8090;
+  }
+}
+EOF
 
-# Install and configure SharingIsCaring init script
-wget https://github.com/serviette/serviette/raw/master/sharingiscaring.init -O /etc/init.d/sharingiscaring
-chmod +x /etc/init.d/sharingiscaring
-mkdir /var/log/sharingiscaring
-chown sharingiscaring:sharingiscaring /var/log/sharingiscaring/
-update-rc.d sharingiscaring defaults
+    ln -s /etc/nginx/sites-available/sharing.serviette.lan /etc/nginx/sites-enabled/sharing.serviette.lan
 
-# Start SharingIsCaring
-service sharingiscaring start
-
+}
 
 #################################################
 # SMTP & IMAP Server/Client
 ################################################# 
 
-# Install Exim, Dovecot and Mutt
-aptitude -y install exim4-light dovecot-imapd mutt
-
-# Create Exim configuration the Debian way
-cat - << EOF > /etc/exim4/update-exim4.conf.conf
+function install_email {
+    # Install Exim, Dovecot and Mutt
+    aptitude -y install exim4-light dovecot-imapd mutt
+    
+    # Create Exim configuration the Debian way
+    cat - << EOF > /etc/exim4/update-exim4.conf.conf
 dc_eximconfig_configtype='local'
 dc_other_hostnames='serviette.lan'
 dc_local_interfaces='127.0.0.1 ; ::1'
@@ -328,9 +373,24 @@ dc_hide_mailname=''
 dc_mailname_in_oh='true'
 dc_localdelivery='mail_spool'
 EOF
+    
+    # Create Exim configuration from configuration template
+    update-exim4.conf
+    
+    # Restart Exim to adopt changes
+    service exim4 restart
+}
 
-# Create Exim configuration from configuration template
-update-exim4.conf
+#install_base
+#configure_network
+#install_httpd
+#install_etherpad
+#install_sharingiscaring
+#install_ikiwiki
+#install_sks
+#install_prosody
+#install_ngircd
+#install_hostapd
+#install_dnsmasq
+#install_email
 
-# Restart Exim to adopt changes
-service exim4 restart
